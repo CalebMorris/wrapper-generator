9ae74259d8e85b6d70126be0ace1cf3b1ccab1a5
Caleb Morris <caleb.morris.g@gmail.com>
2015-06-21 18:09:06 -0700

    Added unit tests to show enforce base and child behaviors
    Input, output, singular calls

--- test/endpointGenerator.js
+++ test/endpointGenerator.js
@@ -4,27 +4,45 @@ import { expect } from 'chai';
 
 import endpointGenerator from '../src/endpointGenerator';
 
+function rand() {
+  return Math.random().toString(36).substring(7);
+}
+
 describe('endpointGenerator', () => {
 
   describe('generate', () => {
 
     it('should allow chaining single child', (done) => {
+      const baseIn = rand();
+      const childIn = rand();
+      const baseReturn = rand();
+      const childReturn = rand();
+
       let baseCount = 0;
       let childCount = 0;
 
-      function base() {
+      function base(inputValue) {
+        expect(inputValue).to.equal(baseIn);
         baseCount++;
+        return baseReturn;
       }
 
-      function child() {
+      function child(inputValue) {
+        expect(inputValue).to.equal(childIn);
         childCount++;
+        return childReturn;
       }
 
       const base = endpointGenerator.generate(base, [child]);
 
-      return Promise.try(base)
-        .then(() => {
-          return base().child();
+      return Promise.try(base, [baseIn])
+        .then((baseValue) => {
+          expect(baseValue).to.equal(baseReturn);
+
+          return base(baseIn).child(childIn)
+        })
+        .then((childValue) => {
+          expect(childValue).to.equal(childReturn);
         })
         .then(() => {
           expect(baseCount).to.equal(1);
