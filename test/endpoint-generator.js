0b130e5e3a9420cfa7eccdedcc80f1b152084f87
Caleb Morris <caleb.morris.g@gmail.com>
2015-06-28 15:12:19 -0700

    Updated tests to work with sinon and added test for unknown # of children

--- test/endpoint-generator.js
+++ test/endpoint-generator.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import Promise from 'bluebird';
 import sinon from 'sinon';
 import { expect } from 'chai';
@@ -6,6 +7,13 @@ import endpointGenerator from '../src/endpoint-generator';
 
 function rand() {
   return Math.random().toString(36).substring(7);
+}
+
+function randInt(lower, higher) {
+  lower = lower || 1;
+  higher = higher || 10;
+
+  return Math.floor((Math.random() * higher) + lower);
 }
 
 describe('endpointGenerator', () => {
@@ -18,24 +26,18 @@ describe('endpointGenerator', () => {
       const baseReturn = rand();
       const childReturn = rand();
 
-      let baseCount = 0;
-      let childCount = 0;
+      const baseStub = sinon.stub();
+      baseStub.withArgs(baseIn).onCall(0).returns(baseReturn);
+      baseStub.returns(null);
 
-      function base(inputValue) {
-        expect(inputValue).to.equal(baseIn);
-        baseCount += 1;
-        return baseReturn;
-      }
+      const childStub = sinon.stub();
+      childStub.withArgs(childIn).onCall(0).returns(childReturn);
+      childStub.returns(null);
 
-      function child(inputValue) {
-        expect(inputValue).to.equal(childIn);
-        childCount += 1;
-        return childReturn;
-      }
+      const base = endpointGenerator.generate(baseStub, { child : childStub });
 
-      const base = endpointGenerator.generate(base, { child });
-
-      return Promise.try(base, [baseIn])
+      return Promise
+        .try(base, [baseIn])
         .then((baseValue) => {
           expect(baseValue).to.equal(baseReturn);
 
@@ -49,8 +51,8 @@ describe('endpointGenerator', () => {
           expect(childValue).to.equal(childReturn);
         })
         .then(() => {
-          expect(baseCount).to.equal(1);
-          expect(childCount).to.equal(1);
+          expect(baseStub.callCount).to.equal(1);
+          expect(childStub.callCount).to.equal(1);
           done();
         })
         .catch(done);
@@ -60,35 +62,25 @@ describe('endpointGenerator', () => {
     it('should allow chaining grand child', (done) => {
       const baseIn = rand();
       const childIn = rand();
-      const grandChildIn = rand();
+      const grandchildIn = rand();
       const baseReturn = rand();
       const childReturn = rand();
       const grandchildReturn = rand();
 
-      let baseCount = 0;
-      let childCount = 0;
-      let grandchildCount = 0;
+      const baseStub = sinon.stub();
+      baseStub.withArgs(baseIn).onCall(0).returns(baseReturn);
+      baseStub.returns(null);
 
-      function base(inputValue) {
-        expect(inputValue).to.equal(baseIn);
-        baseCount += 1;
-        return baseReturn;
-      }
+      const childStub = sinon.stub();
+      childStub.withArgs(childIn).onCall(0).returns(childReturn);
+      childStub.returns(null);
 
-      function child(inputValue) {
-        expect(inputValue).to.equal(childIn);
-        childCount += 1;
-        return childReturn;
-      }
+      const grandchildStub = sinon.stub();
+      grandchildStub.withArgs(grandchildIn).onCall(0).returns(grandchildReturn);
+      grandchildStub.returns(null);
 
-      function grandchild(inputValue) {
-        expect(inputValue).to.equal(grandChildIn);
-        grandchildCount += 1;
-        return grandchildReturn;
-      }
-
-      const child = endpointGenerator.generate(child, { grandchild});
-      const base = endpointGenerator.generate(base, { child });
+      const child = endpointGenerator.generate(childStub, { grandchild : grandchildStub });
+      const base = endpointGenerator.generate(baseStub, { child });
 
       return Promise.try(base, [baseIn])
         .then((baseValue) => {
@@ -103,19 +95,71 @@ describe('endpointGenerator', () => {
         .then((childValue) => {
           expect(childValue).to.equal(childReturn);
 
-          return base(baseIn).child(childIn).grandchild(grandChildIn);
+          return base(baseIn).child(childIn).grandchild(grandchildIn);
         })
         .then((grandChildValue) => {
           expect(grandChildValue).to.equal(grandchildReturn);
         })
         .then(() => {
-          expect(baseCount).to.equal(1);
-          expect(childCount).to.equal(1);
-          expect(grandchildCount).to.equal(1);
+          expect(baseStub.callCount).to.equal(1);
+          expect(childStub.callCount).to.equal(1);
+          expect(grandchildStub.callCount).to.equal(1);
           done();
         })
         .catch(done);
 
+    });
+
+    it('should allow unknown number of descendants', (done) => {
+      const numberOfChildren = randInt(3, 100);
+      let stubs = [];
+      let inputs = [];
+      let outputExpected = [];
+
+      for (let i = 0; i < numberOfChildren; i++) {
+        const input = rand();
+        const output = rand();
+        const stub = sinon.stub();
+        stub.withArgs(input).onCall(0).returns(output);
+        stub.returns(null);
+        stub.position = i;
+
+        outputExpected.push(output);
+        inputs.push(input);
+        stubs.push(stub);
+      }
+
+      let callPromise = endpointGenerator.generate(
+        stubs[numberOfChildren - 2],
+        { [numberOfChildren - 1] : stubs[numberOfChildren - 1] }
+      );
+
+      for (let i = stubs.length - 2; i > 0; i--) {
+        callPromise = endpointGenerator.generate(stubs[i - 1], { [i] : callPromise });
+      }
+
+      let currentResult = callPromise(inputs[0]);
+
+      for (let i = 1; i < stubs.length; i++) {
+        try {
+          currentResult = currentResult[i](inputs[i]);
+        } catch(err) {
+          throw new Error('[i:' + i + '] :' + err.toString());
+        }
+      }
+
+      return currentResult.then((finalResult) => {
+        expect(finalResult).to.equal(outputExpected[inputs.length - 1]);
+      })
+      .then(() => {
+        return Promise.each(_.dropRight(stubs, 1), (stub, key) => {
+          expect(stub.callCount).to.equal(0, 'Failed on #' + key);
+        }).then(() => {
+          expect(stubs[numberOfChildren - 1].callCount).to.equal(1);
+        });
+      })
+      .then(done)
+      .catch(done);
     });
 
   });
